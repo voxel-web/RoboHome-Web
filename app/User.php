ff86ab33dd4fba48d380a3138bb8f8582ab6cd4e
Drew Budwin <drew@budw.in>
2017-10-08 21:18:15 -0400

    Added functions to the User model

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
@@ -26,4 +27,19 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function add(string $name, string $email, string $userId): User
+    {
+        $this->name = $name;
+        $this->email = $email;
+        $this->user_id = $userId;
+        $this->save();
+
+        return $this;
+    }
+
+    public function devices(): HasMany
+    {
+        return $this->hasMany(Device::class);
+    }
 }
