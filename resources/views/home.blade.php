d5163c12128006bf9a5e081fa66928cd96682bf5
Drew Budwin <drew@budw.in>
2017-10-08 21:22:31 -0400

    Adding views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+
+<div class="container-fluid">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+        <div class="col-sm-12">
+            <div class="container">
+                <div class="jumbotron">
+                    <h2>
+                        Welcome to RoboHome
+                    </h2>
+                    <p>
+                        RoboHome is a SaaS tool that also integrates with Amazon's Echo to enable control of semi-connected devices (think IR, and RF) in your house over wifi!
+                    </p>
+                    <p>
+                        <a class="btn btn-primary btn-large" href="{{ route('login') }}" id="LoginWithAmazon"><span class="glyphicon glyphicon-log-in" aria-hidden="true"></span> Log In</a>
+                        <a href="{{ route('register') }}">Register</a>
+                    </p>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
 @endsection
