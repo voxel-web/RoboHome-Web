f41756ee3746486e69b6a693597a4918608fa74f
Drew Budwin <drew@budw.in>
2017-10-08 21:17:52 -0400

    Change where the default auth controllers redirect to

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/devices';
 
     /**
      * Create a new controller instance.
