d48b3a9035f3a7bfadca8a1c75e892e56a60e548
Drew Budwin <drew@budw.in>
2017-10-08 20:33:53 -0400

    Routes

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//https://stackoverflow.com/questions/39196968/laravel-5-3-new-authroutes
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('/', 'Web\LoginController@index')->name('index');
+Route::get('/login', 'Web\LoginController@login')->name('login');
+Route::get('/logout', 'Web\LoginController@logout')->name('logout');
+
+Route::get('/devices', 'Web\DevicesController@devices')->name('devices');
+Route::post('/devices/add', 'Web\DevicesController@add')->name('addDevice');
+Route::get('/devices/delete/{id}', 'Web\DevicesController@delete')->name('deleteDevice');
+Route::post('/devices/update/{id}', 'Web\DevicesController@update')->name('updateDevice');
+Route::post('/devices/{action}/{id}', 'Web\DevicesController@handleControlRequest')
+    ->where(['action' => '[a-z]+'])
+    ->name('handleControlRequest');
